56986dd0dee4a8f8f1f38ba884d4e499cde8e5eb
Abhiraj Rajput <abhirajrajput666@gmail.com>
2026-03-19 23:01:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import { info } from './data';
+
+export default function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <footer>© 2025 {info.name}. All rights reserved.</footer>
+    </div>
+  );
+}
